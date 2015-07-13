85883cbd08ad203638fc75cfd890c12184a1f6b9
Mike Liu <mikeliu@uber.com>
2015-07-12 17:23:52 -0700

    refactor

--- app/src/main/java/com/safrzone/safrzone/services/ServiceConstants.java
+++ app/src/main/java/com/safrzone/safrzone/services/ServiceConstants.java
@@ -2,8 +2,8 @@ package com.safrzone.safrzone.services;
 
 public class ServiceConstants {
 
-    public static final String AccessToken = "pk.eyJ1IjoibWlrZXpsaXUiLCJhIjoiLUc0QjVKVSJ9.8wiO_mNGaS3HDKEOPa6AQw";
-    public static final String MapBoxId = "mikezliu.mm4o1j45";
-    public static final String MapBoxApiHost = "http://api.tiles.mapbox.com";
-    public static final String SafrZoneApiHost = "http://safrzone-env.elasticbeanstalk.com";
+    public static final String AccessToken = "";
+    public static final String MapBoxId = "";
+    public static final String MapBoxApiHost = "";
+    public static final String SafrZoneApiHost = "";
 }
